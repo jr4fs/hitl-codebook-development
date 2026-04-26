3d062781a646f8df4662fe7a406cd19852d6b61b
Swaminathan Chellappa <schellap@a04-15.hpc.usc.edu>
2026-04-26 12:32:29 -0700

    updated frontend config file and gitignore

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: true,//['desiree-flamba-colorfully.ngrok-free.dev', 'all'],
+    proxy: {
+      '/api': 'http://localhost:8080',
+      '/embedding': 'http://localhost:8000',
+      '/anonymize': 'http://localhost:8000',
+      '/chat': 'http://localhost:8000',
+      '/inference': 'http://localhost:8000',
+    }
+  },
 })
